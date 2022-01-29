1725e60d37cbfa0fe6953b5b9f56ad4c29242e50
Max <maximofain0@gmail.com>
2022-01-29 15:46:29 -0300

    Heart and Life Player System :3

---
+++ Assets/Scripts/HearthSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HearthSystem : MonoBehaviour
+{
+    [SerializeField] GameObject[] _hearts;
+    [SerializeField] GameObject _player;
+
+    private int _life;
+    // Update is called once per frame
+    void Update()
+    {
+        int _life = _player.GetComponent<PlayerDeath>()._playerHP;
+
+        if (_life < 1)
+        {
+            Destroy(_hearts[0].gameObject);
+        }else if (_life < 2)
+        {
+            Destroy(_hearts[1].gameObject);
+        }else if (_life < 3)
+        {
+            Destroy(_hearts[2].gameObject);
+        }
+        
+    }
+}
