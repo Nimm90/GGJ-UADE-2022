06a7e88b22c856dd09c52f416c6b851570b9e531
Max <maximofain0@gmail.com>
2022-01-29 17:26:45 -0300

    Platform Player Death

---
+++ Assets/Scripts/PlatformPlayerDeath.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformPlayerDeath : MonoBehaviour
+{
+
+    [SerializeField] GameObject _platformerPlayer;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        Destroy(_platformerPlayer);
+    }
+
+}
