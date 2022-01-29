0674058989c2d0de3407af480698c994fc753a44
eruandou <zambra_nacho@hotmail.com>
2022-01-29 11:16:04 -0300

    Working MenuScene

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject creditsScreen;
+    [SerializeField] private GameObject mainScreen;
+
+    [SerializeField] private string sceneToLoad;
+
+    public event Action OnToggle;
+    private void Awake()
     {
+        Init();
+    }
+
+    private void Init()
+    {
+        creditsScreen.SetActive(false);
+        mainScreen.SetActive(true);
+    }
+
+    public void Toggler()
+    {
+        creditsScreen.SetActive(!creditsScreen.activeSelf);
+        mainScreen.SetActive(!mainScreen.activeSelf);
+    }
+
+    public void GoToPlay()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+        return;
+        #endif
+        
+        Application.Quit();
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
