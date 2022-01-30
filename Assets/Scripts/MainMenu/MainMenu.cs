748377ef32132be69e789d9a0491d9a8afb1c221
Nicolás Magno <nicolas.magno.maceira@gmail.com>
2022-01-30 18:25:21 -0300

    Added support for various screens in main menu
    -Added About & Controls Screens in Main Menu

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject creditsScreen;
+    /*[SerializeField] private GameObject creditsScreen;
     [SerializeField] private GameObject mainScreen;
+    [SerializeField] private GameObject aboutScreen;*/
 
     [SerializeField] private string sceneToLoad;
+
+    [SerializeField] private GameObject[] screens;
+    [SerializeField] private GameObject initialScreen;
 
     public UnityEvent OnAnyButtonPress;
 
@@ -19,15 +23,61 @@ public class MainMenu : MonoBehaviour
 
     private void Init()
     {
-        creditsScreen.SetActive(false);
+        SetAllScreenInactive();
+
+        initialScreen.SetActive(true);
+
+        /*creditsScreen.SetActive(false);
         mainScreen.SetActive(true);
+        aboutScreen.SetActive(false);*/
     }
 
-    public void Toggler()
+    /*public void Toggler()
     {
         creditsScreen.SetActive(!creditsScreen.activeSelf);
         mainScreen.SetActive(!mainScreen.activeSelf);
         OnAnyButtonPress?.Invoke();
+    }*/
+
+    /*public void SetCreditsScreenActive()
+    {
+        creditsScreen.SetActive(true);
+        mainScreen.SetActive(false);
+        aboutScreen.SetActive(false);
+
+        OnAnyButtonPress?.Invoke();
+    }
+
+    public void SetAboutScreenActive()
+    {
+        creditsScreen.SetActive(false);
+        mainScreen.SetActive(false);
+        aboutScreen.SetActive(true);
+
+        OnAnyButtonPress?.Invoke();
+    }
+
+    public void SetMainScreenActive()
+    {
+        creditsScreen.SetActive(false);
+        mainScreen.SetActive(true);
+        aboutScreen.SetActive(false);
+
+        OnAnyButtonPress?.Invoke();
+    }*/
+
+    public void SetScreenActive(GameObject screen)
+    {
+        SetAllScreenInactive();
+        screen.SetActive(true);
+
+        OnAnyButtonPress?.Invoke();
+    }
+
+    private void SetAllScreenInactive()
+    {
+        foreach (var s in screens)
+            s.SetActive(false);
     }
 
     public void GoToPlay()
