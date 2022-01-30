a7540f9b99fc33240ace0a65eb775a7ac310e88e
eruandou <zambra_nacho@hotmail.com>
2022-01-30 14:46:11 -0300

    AudioLerp

---
+++ Assets/Scripts/AudioLerper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class AudioLerper : MonoBehaviour
+{
+    [SerializeField] private AudioSource normalAudio;
+    [SerializeField] private AudioSource reverseAudio;
+    [SerializeField] private AnimationCurve audioAnimationCurve;
+    [SerializeField] private float audioLerpTime;
+    private Coroutine _lerpAudioCoroutine;
+
+
+    public void LerpAudio(bool toReverse)
+    {
+        if (_lerpAudioCoroutine != null)
+        {
+            StopCoroutine(_lerpAudioCoroutine);
+        }
+
+        _lerpAudioCoroutine = StartCoroutine(LerpAudioCoroutine(toReverse));
+    }
+
+    private IEnumerator LerpAudioCoroutine(bool toReverse)
+    {
+        float counter = 0;
+        var fromAudioSource = toReverse ? normalAudio : reverseAudio;
+        var toAudioSource = toReverse ? reverseAudio : normalAudio;
+        var fromVolume = fromAudioSource.volume;
+        var toVolume = toAudioSource.volume;
+
+        while (counter < audioLerpTime)
+        {
+            var lerpAmount = counter / audioLerpTime;
+            var lerpCurve = audioAnimationCurve.Evaluate(lerpAmount);
+            fromAudioSource.volume = Mathf.Lerp(fromVolume, 0, lerpCurve);
+            toAudioSource.volume = Mathf.Lerp(toVolume, 1, lerpAmount);
+            counter += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
